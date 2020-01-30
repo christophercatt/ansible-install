428ab4d29951335986dedbfed093b1c3109ebd8a
Christopher Catt <ccatt601@gmail.com>
2020-01-30 19:27:44 +0000

    Addition of ansible-go command creation and separation of script into functions for modularity

--- ans-install.sh
+++ ans-install.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 
+## This is a lightweight install script to install Ansible within a virtual environment.
+## To change install location, simply change the path within the $dir variable.
+
+
+
+## VARIABLES ##
 
 ## Customisable install location
 dir="/home/ansible-play"
+
+
+
+## FUNCTIONS ##
+
+## Function to setup virtual environment and install Ansible within
+setup_venv_ansible () {
+
+        ## Setup virtual environment
+        python3.7 -m venv venv
+        source venv/bin/activate
+        ## Install Ansible packages
+        pip install --upgrade pip
+        pip install ansible
+
+}
+
+## Function to add ansible-go command to global .bashrc
+add_ansible_go () {
+
+	## Add ansible-go alias into .bashrc file
+	echo "alias ansible-go='cd $dir && source venv/bin/activate'" >> ~/.bashrc
+	## Source .bashrc file to enable instant use of ansible-go command
+	source ~/.bashrc
+
+}
+
+
+
+## MAIN ##
 
 ## Checks if install directory exists
 if [ ! -d "$dir" ]
@@ -13,15 +49,12 @@ then
 	## Install dependencies and create empty hosts file for later use
 	apt install -y python3.7 python3.7-dev python3.7-venv python3-venv
 	touch hosts
-	## Setup virtual environment
-	python3.7 -m venv venv
-	source venv/bin/activate
-	## Install Ansible packages
-	pip install --upgrade pip
-	pip install ansible
+	## Setup virtual environment and install Ansible
+	setup_venv_ansible
 
 else
-
+	## CD to install directory
+	cd "$dir"
 	## If install directory already exists, check status of hosts file
 	if [ ! -f "$dir/hosts" ]
 	then
@@ -32,14 +65,12 @@ else
 	## Checks if virtual environment environmental variable has been set
 	if [[ "$VIRTUAL_ENV" != "" ]]
 	then
-		## Upgrade Ansible if ne updates available
+		## Upgrade Ansible if new updates available
+		source venv/bin/activate
 		pip install --upgrade ansible
 	else
-		## If venv environmental variable doesn't exist, setup venv
-		python3.7 -m venv venv
-		source venv/bin/activate
-		## Install Ansible packages
-		pip install --upgrade pip
-		pip install ansible
+	        ## Setup virtual environment and install Ansible
+	        setup_venv_ansible
+
 	fi
 fi
