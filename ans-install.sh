de513cb35a49e993e5f419fa6d7932b33bb85b5d
Christopher Catt <ccatt601@gmail.com>
2020-01-30 09:28:39 +0000

    Initial install script

---
+++ ans-install.sh
@@ -0,0 +1,27 @@
+!#/bin/bash
+
+dir="/home/ansible-play"
+
+if [ ! -d "$dir" ]
+then
+	mkdir "$dir" && cd "$dir"
+	apt-get install -y python2.7 python-pip
+	pip install -U virtualenv
+
+	virtualenv .venv
+	source .venv/bin/activate
+	pip install ansible
+
+	touch hosts
+else
+	if [[ "$VIRTUAL_ENV" != "" ]]
+	then
+		pip install -U ansible
+	else
+		virtualenv .venv
+		source .venv/bin/activate
+		pip install ansible
+
+		touch hosts
+	fi
+fi
